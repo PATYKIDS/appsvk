158cf3639542a014e20a4d340229b937aa5f9786
appsvk <admin@patykids.ru>
2019-04-30 18:59:42 +0400

    Updates

---
+++ precache-manifest.12e080997ddb8642dd9c4fcbb3a3242f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "720a6932c3434c4866c8",
+    "url": "/appsvk/static/js/runtime~main.720a6932.js"
+  },
+  {
+    "revision": "de100e03207f9a03a019",
+    "url": "/appsvk/static/js/main.de100e03.chunk.js"
+  },
+  {
+    "revision": "d64d221229b81b6fc607",
+    "url": "/appsvk/static/js/2.d64d2212.chunk.js"
+  },
+  {
+    "revision": "d64d221229b81b6fc607",
+    "url": "/appsvk/static/css/2.15189452.chunk.css"
+  },
+  {
+    "revision": "f312717b421eabf40a2bf7e70bc64872",
+    "url": "/appsvk/index.html"
+  }
+];
